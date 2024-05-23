f3e465216ae99a82ef9ef25bf2df1248ec63d2f3
add33m <48681209+add33m@users.noreply.github.com>
2024-05-24 01:24:10 +0200

    Cleaned up and updated dependencies

--- rust_examples/examples/primes_test_extint.rs
+++ rust_examples/examples/primes_test_extint.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-// Fix for a weird issue?
 #![feature(type_alias_impl_trait)]
 
 use core::panic::PanicInfo;
@@ -37,34 +36,37 @@ mod app {
     #[local]
     struct Local {
         light: i32,
-        pin0: Pin0,
-        pin1: Pin1,
-        pin2: Pin2,
-        pin3: Pin3,
+        increase: bool,
+        pin0: Pout0,
+        pin1: Pout1,
+        pin2: Pout2,
+        pin3: Pout3,
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         let peripherals = cx.device;
 
-        let pins = peripherals.gpio.pins();
-        let pin0 = pins.pin0;
-        let pin1 = pins.pin1;
-        let pin2 = pins.pin2;
-        let pin3 = pins.pin3;
+        let pins = peripherals.gpo.split();
+        let pin0 = pins.pout0;
+        let pin1 = pins.pout1;
+        let pin2 = pins.pout2;
+        let pin3 = pins.pout3;
 
         let light = 0;
+        let increase = true;
         let timer = peripherals.timer;
 
         timer.write(0b100000000001110); // interrupt every (1024 << 14) cycles, at 20Mhz yields
                                         // ~1.19Hz
-
+                                        //timer.write(0b101110);
         rtic::export::pend(interrupt1::Interrupt1);
 
         (
             Shared { dummy: 0 },
             Local {
                 light,
+                increase,
                 pin0,
                 pin1,
                 pin2,
@@ -72,37 +74,9 @@ mod app {
             },
         )
     }
-
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {}
-    }
-
-    // This interrupt task will switch between led0-2 in a cycle
-    #[task(binds = Interrupt0, priority = 3, shared = [dummy], local = [light, pin0, pin1, pin2])]
-    fn i0(cx: i0::Context) {
-        // Turn off all lights
-        cx.local.pin0.set_low();
-        cx.local.pin1.set_low();
-        cx.local.pin2.set_low();
-
-        // Turn on the correct light
-        if *cx.local.light == 0 {
-            cx.local.pin0.set_high();
-        } else if *cx.local.light == 1 {
-            cx.local.pin1.set_high();
-        } else if *cx.local.light == 2 {
-            cx.local.pin2.set_high();
-        }
-
-        // Increment light to be turned on for next interrupt, modulo 3 for 3 lights
-        *cx.local.light += 1;
-        *cx.local.light %= 3;
-    }
-
     // This background task will toggle led3 every time it finds a new prime
-    #[task(binds = Interrupt1, priority = 1, shared = [dummy], local = [pin3])]
-    fn primes(cx: primes::Context) {
+    #[idle(local = [pin3])]
+    fn idle(cx: idle::Context) -> ! {
         let mut n = 30000;
         let mut highest_prime_so_far;
         let mut blink = false;
@@ -121,6 +95,32 @@ mod app {
             }
             n += 1;
         }
+    }
+
+    // This interrupt task will switch between led0-2 in a cycle
+    #[task(binds = Interrupt0, priority = 3, shared = [dummy], local = [light, increase, pin0, pin1, pin2])]
+    fn i0(cx: i0::Context) {
+        // Turn off all lights
+        cx.local.pin0.set_low();
+        cx.local.pin1.set_low();
+        cx.local.pin2.set_low();
+
+        // Turn on the correct light
+        if *cx.local.light == 0 {
+            cx.local.pin0.set_high();
+        } else if *cx.local.light == 1 {
+            cx.local.pin1.set_high();
+        } else if *cx.local.light == 2 {
+            cx.local.pin2.set_high();
+        }
+
+        // Increment light to be turned on for next interrupt, modulo 3 for 3 lights
+        if *cx.local.increase {
+            *cx.local.light += 1;
+        } else {
+            *cx.local.light -= 1;
+        }
+        *cx.local.light %= 3;
     }
 }
 
